a17337c0aa4f2b824abe1cbba82fcf477281bc36
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-03-24 21:32:22 +0700

    Create biodata.java

---
+++ SourcePraktikum/Modul2/Praktikum/biodata.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author alika
+ */
+public class biodata {
+    public static void main(String args[]) {
+        System.out.println("NIM: 2023081");
+        System.out.println("Nama: Alika Fadila Haya");
+        System.out.println("Kelas: TINFC-2023-02");
+        System.out.println("Program Studi: Teknik Informatika");
+        System.out.println("Alamat: Ds. Cidulang Kec. Cikijing Kab. Majalengka");
+        System.out.println("Email: alikafadilahaya18@gmail.com");
+        System.out.println("No.HP: +6285156472095^");
+    }
+}
