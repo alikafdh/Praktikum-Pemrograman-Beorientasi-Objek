778f59215e2ba12148f475f3843bff32e159058c
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-03-24 21:09:59 +0700

    Program Java

---
+++ SourcePraktikum/Modul1/Praktikum1/praktikum1.java
@@ -0,0 +1,10 @@
+public class praktikum1 {
+
+    public static void main(String args[]) {
+    /*Program Java PBO Pertamaku*/
+    System.out.println("Pemrograman Berorientasi Objek");
+    System.out.print("Java Is Simple, ");
+    System.out.print("Java Is Simple, ");
+
+    }
+}
