3360ca228837113007036d4742ddf92020aa11ad
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-03-24 21:11:08 +0700

    Program Java

---
+++ SourcePraktikum/Modul1/Praktikum2/praktikum2.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author alika
+ */
+public class Praktikum2 {
+    public static void main(String args[]) {
+        int a,b,c,d;
+        a=5;
+        b=3;
+        c=6;
+        d=a+b;
+        System.out.println("Nilai c = "+c);
+        System.out.println("Nilai a + b = "+d);
+    }
+}
