0c70011217ac376ac473092d1676f2bfa4f14b84
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-03-31 23:00:53 +0700

    Update volumebalok.java

--- SourcePraktikum/Modul2/TugasMandiri/volumebalok.java
+++ SourcePraktikum/Modul2/TugasMandiri/volumebalok.java
@@ -1,1 +1,16 @@
-
+import javax.swing.*;
+//deklarasi class
+public class volumebalok {
+    //deklarasi method utama
+    public static void main(String[] args) {
+        double volume;
+        //instan objek dari class balok
+        balok b1 = new balok();        
+        b1.panjang = Double.parseDouble(JOptionPane.showInputDialog("Masukan Panjang Balok : "));      
+        b1.lebar = Double.parseDouble(JOptionPane.showInputDialog("Masukan Lebar Balok : ")) ;      
+        b1.tinggi = Double.parseDouble(JOptionPane.showInputDialog("Masukan Tinggi Balok : "));
+        //hitung
+        volume = b1.panjang*b1.lebar*b1.tinggi;
+        System.out.println("Volume Balok = "+volume+" Cm3");
+    }
+}
