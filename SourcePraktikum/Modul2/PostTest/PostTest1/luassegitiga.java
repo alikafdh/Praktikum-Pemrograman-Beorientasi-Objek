a43dbd3110962cba25e2d7666b4c984d3e0eb5b8
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-03-31 22:30:48 +0700

    Create luassegitiga.java

---
+++ SourcePraktikum/Modul2/PostTest/PostTest1/luassegitiga.java
@@ -0,0 +1,10 @@
+public class luassegitiga {
+    public static void main(String[] args) {
+        double luas;
+        segitiga s1 = new segitiga();
+        s1.alas = 4;
+        s1.tinggi = 10;
+        luas = (s1.alas*s1.tinggi)/2;
+        System.out.println("Luas Segitiga = "+luas+"Cm2");
+    }
+}
