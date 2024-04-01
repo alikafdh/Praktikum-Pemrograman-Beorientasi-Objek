a567a23d1b55b4cb65f90e60b959c852b48e75a8
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-04-01 17:54:57 +0700

    Create Manusia.java

---
+++ SourcePraktikum/Modul3/Praktikum/Praktikum3/Manusia.java
@@ -0,0 +1,15 @@
+public class Manusia {
+    public String nama;
+    public Manusia(String n){
+        this.nama = n;
+    }
+    public String tampilkanNama(){
+        return nama;
+    }
+    public void makan(){
+        System.out.println("Nyam...Nyam...Nyam...");
+    }
+    public void kerja(){
+        System.out.println("Kerja...Kerjaaa...");
+    }
+}
