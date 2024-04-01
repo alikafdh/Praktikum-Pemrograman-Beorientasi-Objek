cee136c09090b39d8046fba223ee1ba801c582ee
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-04-01 17:55:59 +0700

    Create Jaka.java

---
+++ SourcePraktikum/Modul3/Praktikum/Praktikum3/Jaka.java
@@ -0,0 +1,7 @@
+class Jaka {
+    public static void main(String[] args) {
+    Manusia andi=new Manusia("Jaka");
+        System.out.println("Nama = "+andi.tampilkanNama());
+        andi.makan();
+}
+}
