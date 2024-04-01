544145d13dad8ccd9f8ebb87ca0a48f74409890e
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-04-01 17:56:59 +0700

    Create SPP.java

---
+++ SourcePraktikum/Modul3/Praktikum/Praktikum4/SPP.java
@@ -0,0 +1,16 @@
+public class SPP {
+    String nama, nim;
+    int spt,spv,sks;
+    
+    public static void main(String[] args) {
+    this.nama= nama;
+    this.nim= nim;
+    this.spt= spt;
+    this.spv= spv;
+    this.sks= sks;
+    }
+    
+    public int hitung(){
+        return (spt+(spv*sks));
+    }
+}
