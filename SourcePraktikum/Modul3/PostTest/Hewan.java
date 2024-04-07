a8f6fc4800c6feb9d842172372c5051136fe940e
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-04-07 21:46:59 +0700

    Update Hewan.java

--- SourcePraktikum/Modul3/PostTest/Hewan.java
+++ SourcePraktikum/Modul3/PostTest/Hewan.java
@@ -1,1 +1,15 @@
-
+public class Hewan {
+    public String jenis;
+    public Hewan(String n){
+        this.jenis = n;
+    }
+    public String tampilkanJenis(){
+        return jenis;
+    }
+    public void suara(){
+        System.out.println("meow..meow...meow...");
+    }
+    public void berjalan(){
+        System.out.println("berjalan dengan 4 kaki");
+    }
+}
