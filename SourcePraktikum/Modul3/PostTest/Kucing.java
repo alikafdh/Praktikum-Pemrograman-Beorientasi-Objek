46b8cfa9cf5cadcb9b1c126ffb132cd89dc7215c
Alika Fadila Haya <126769226+alikafdh@users.noreply.github.com>
2024-04-07 21:47:25 +0700

    Create Kucing.java

---
+++ SourcePraktikum/Modul3/PostTest/Kucing.java
@@ -0,0 +1,7 @@
+class Kucing {
+    public static void main(String[] args) {
+    Hewan hwn1=new Hewan("Kucing");
+        System.out.println("Jenis Hewan = "+hwn1.tampilkanJenis());
+        hwn1.suara();
+}
+}
